cd7dcfb228d069f227f972dafcf2f20ba99fd574
mohamed hasan <87263653+mo7medhasan@users.noreply.github.com>
2026-05-03 21:06:18 +0300

    feat: add Android MainActivity implementation and configure Gradle daemon JVM toolchain properties

--- android/app/src/main/java/com/smartagentapp/MainActivity.kt
+++ android/app/src/main/java/com/smartagentapp/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.smartagentapp
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(null)
-  }
+
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
   override fun getMainComponentName(): String = "SmartAgentApp"
 
   /**
